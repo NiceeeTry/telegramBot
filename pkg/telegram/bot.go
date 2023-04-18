5eb44eaafd5edd6e1b8a6547a5c63f32a31d9cde
Yo <yo>
2023-04-18 18:52:19 +0600

    next

--- pkg/telegram/bot.go
+++ pkg/telegram/bot.go
@@ -2,22 +2,25 @@ package telegram
 
 import (
 	"log"
+	"telegram-bot/pkg/repository"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/zhashkevych/go-pocket-sdk"
 )
 
 type Bot struct {
-	bot          *tgbotapi.BotAPI
-	pocketClient *pocket.Client
-	redirectURL  string
+	bot             *tgbotapi.BotAPI
+	pocketClient    *pocket.Client
+	tokenRepository repository.TokenRepository
+	redirectURL     string
 }
 
-func NewBot(bot *tgbotapi.BotAPI, pocketClient *pocket.Client, redirectURL string) *Bot {
+func NewBot(bot *tgbotapi.BotAPI, pocketClient *pocket.Client, tr repository.TokenRepository, redirectURL string) *Bot {
 	return &Bot{
-		bot:          bot,
-		pocketClient: pocketClient,
-		redirectURL:  redirectURL,
+		bot:             bot,
+		pocketClient:    pocketClient,
+		redirectURL:     redirectURL,
+		tokenRepository: tr,
 	}
 }
 
